4691e78dbe64a5e1c9275033b6c7a5391390a0cc
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-05-03 17:26:10 +0300

    Fix exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,16 +1,10 @@
 class SendMessageError(Exception):
     """Класс исключения для обработки ошибок во время отправки сообщения."""
 
-    pass
-
 
 class StatusCodeIsNot200Error(Exception):
     """Класс исключения для обработки ошибок во время запроса к API."""
 
-    pass
-
 
 class UnavailableAPIError(Exception):
     """Класс исключения для обработки ошибки недоступности API-сервиса."""
-
-    pass
