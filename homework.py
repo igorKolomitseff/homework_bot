e5ca087b0882d180c72a118acea650aa03d8a938
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-05-05 22:13:43 +0300

    Fix homework.py

--- homework.py
+++ homework.py
@@ -15,7 +15,7 @@ from exceptions import (
 
 load_dotenv()
 
-PRACTICUM_TOKEN =  os.getenv('PRACTICUM_TOKEN')
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
